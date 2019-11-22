3ae8b632eea1247399a23fb35af6c369c80a487d
Jerome <jerome.hittle@colorado.edu>
2019-11-21 22:52:20 -0700

    Committing an initial program to the repository.  Program uses HFXTAL to set a 2Hz blink on P1.0 and a simple PWM strobe on P2.4.

---
+++ Software/ESDFinalProject/systemClocks.h
@@ -0,0 +1,15 @@
+/*
+ * systemClocks.h
+ *
+ *  Created on: Nov 21, 2019
+ *      Author: Jerome
+ */
+
+#ifndef SYSTEMCLOCKS_H_
+#define SYSTEMCLOCKS_H_
+
+
+
+void initSystemClocks(void);
+
+#endif /* SYSTEMCLOCKS_H_ */
